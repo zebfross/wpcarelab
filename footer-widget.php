eaa51ba8a7e6caac394f719d52023c2ea4b0b168
zebfross <zebfross@hotmail.com>
2021-04-19 16:44:47 -0700

    add custom bootswatch sass; add footer widget

--- footer-widget.php
+++ footer-widget.php
@@ -1,17 +1,20 @@
 <?php
 
-if ( is_active_sidebar( 'footer-1' ) || is_active_sidebar( 'footer-2' ) || is_active_sidebar( 'footer-3' ) ) {?>
+if ( is_active_sidebar( 'footer-1' ) || is_active_sidebar( 'footer-2' ) || is_active_sidebar( 'footer-3' ) || is_active_sidebar('footer-4') ) {?>
         <div id="footer-widget" class="row m-0 <?php if(!is_theme_preset_active()){ echo 'bg-light'; } ?>">
             <div class="container">
                 <div class="row">
                     <?php if ( is_active_sidebar( 'footer-1' )) : ?>
-                        <div class="col-12 col-md-4"><?php dynamic_sidebar( 'footer-1' ); ?></div>
+                        <div class="col-12 col-md-3"><?php dynamic_sidebar( 'footer-1' ); ?></div>
                     <?php endif; ?>
                     <?php if ( is_active_sidebar( 'footer-2' )) : ?>
-                        <div class="col-12 col-md-4"><?php dynamic_sidebar( 'footer-2' ); ?></div>
+                        <div class="col-12 col-md-3"><?php dynamic_sidebar( 'footer-2' ); ?></div>
                     <?php endif; ?>
                     <?php if ( is_active_sidebar( 'footer-3' )) : ?>
-                        <div class="col-12 col-md-4"><?php dynamic_sidebar( 'footer-3' ); ?></div>
+                        <div class="col-12 col-md-3"><?php dynamic_sidebar( 'footer-3' ); ?></div>
+                    <?php endif; ?>
+                    <?php if ( is_active_sidebar( 'footer-4' )) : ?>
+                        <div class="col-12 col-md-3"><?php dynamic_sidebar( 'footer-4' ); ?></div>
                     <?php endif; ?>
                 </div>
             </div>
--- functions.php
+++ functions.php
@@ -153,6 +153,15 @@ function wp_bootstrap_starter_widgets_init() {
         'before_title'  => '<h3 class="widget-title">',
         'after_title'   => '</h3>',
     ) );
+    register_sidebar( array(
+        'name'          => esc_html__( 'Footer 4', 'wp-bootstrap-starter' ),
+        'id'            => 'footer-4',
+        'description'   => esc_html__( 'Add widgets here.', 'wp-bootstrap-starter' ),
+        'before_widget' => '<section id="%1$s" class="widget %2$s">',
+        'after_widget'  => '</section>',
+        'before_title'  => '<h3 class="widget-title">',
+        'after_title'   => '</h3>',
+    ) );
 }
 add_action( 'widgets_init', 'wp_bootstrap_starter_widgets_init' );
 
